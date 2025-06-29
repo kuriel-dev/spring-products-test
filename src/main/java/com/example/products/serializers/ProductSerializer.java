5316cec5ea0af91ac16175ff4e5b939a8dca7d6f
Kuriel <ikuriel@gmail.com>
2025-06-29 01:32:59 -0600

    CI/CD GCP GitHub Actions

--- src/main/java/com/example/products/serializers/ProductSerializer.java
+++ src/main/java/com/example/products/serializers/ProductSerializer.java
@@ -1,12 +1,9 @@
 package com.example.products.serializers;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public abstract class ProductSerializer {
-//    @JsonIgnore
-    public abstract Long getId();
 
     @JsonProperty("desc")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
